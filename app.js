4fd6e534a8df8370c7e576ed0daf10814a573685
yanshijie <shijie.Yan@mkcorp.com>
2019-10-22 17:01:31 +0800

    update

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ module.exports = app => {
 
   if (options.graphiql === true) {
     router.all('/graphiql', GraphqlPG({
-        endpointURL: graphQLRouter
+        endpoint: graphQLRouter
     }));
   }
 
